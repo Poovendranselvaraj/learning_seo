29d9da8453d743a19613158b4ad55e5cef6c1929
Poovendranselvaraj <poovendranselvaraj333@gmail.com>
2024-10-22 00:06:05 +0530

    36.44

--- nextjs/app/(root)/layout.tsx
+++ nextjs/app/(root)/layout.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import Footer from '@/components/ui/Footer'
+import Navbar from '@/components/ui/Navbar'
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      Navbar
+      <Navbar />
       {children}
-      Footer
+      <Footer />
     </>
   )
 }
