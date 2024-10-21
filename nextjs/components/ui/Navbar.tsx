65318343ae7e4efb42c9236faf55757f6a0cd979
Poovendranselvaraj <poovendranselvaraj333@gmail.com>
2024-10-22 00:17:21 +0530

    38.08

--- nextjs/components/ui/Navbar.tsx
+++ nextjs/components/ui/Navbar.tsx
@@ -7,13 +7,25 @@ const Navbar = () => {
     <nav className="flex-center fixed top-0 z-50 w-full border-b-2 border-black-200 py-7 text-white">
       <div className="flex-between mx-auto w-full max-w-screen-2xl px-6 xs:px-8 sm:px-16">
          <Link href='/'>
-             <Image src="/json-logo.svg"
-             alt="json-logo"
-             width={55} 
-             height={40}
+            <Image src='/jsm-logo.svg'
+                width={55} 
+                height={40}
+                alt="JSM logo"
             />
          
          </Link>
+
+         <Image src='/hamburger-menu.svg'
+                width={55} 
+                height={40}
+                alt="Hamburger-menu"
+                className='block md:hidden'
+        />
+        <ul className='flex-center gap-x-3'>
+
+        </ul>
+
+
       </div>
     </nav>
   )
