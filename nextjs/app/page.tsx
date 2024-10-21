bb5e902e8704b24eec588bf7f23c24e181f76018
Poovendranselvaraj <poovendranselvaraj333@gmail.com>
2024-10-21 23:17:15 +0530

    install

--- nextjs/app/page.tsx
+++ nextjs/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button"
 export default function Home() {
   return (
     <div>
-      <h1 className="text-2xl text-underline">Home</h1>
+      <h1 className="text-2xl underline">Home</h1>
       <Button>Click me</Button>
     </div>
   )
