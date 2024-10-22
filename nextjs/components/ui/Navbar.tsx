193722cc328e46329628e89e8a58576ce8255db4
Poovendranselvaraj <poovendranselvaraj333@gmail.com>
2024-10-22 10:38:38 +0530

    40.00

--- nextjs/components/ui/Navbar.tsx
+++ nextjs/components/ui/Navbar.tsx
@@ -21,7 +21,17 @@ const Navbar = () => {
                 alt="Hamburger-menu"
                 className='block md:hidden'
         />
-        <ul className='flex-center gap-x-3'>
+        <ul className='flex-center gap-x-3 max-md:hidden md:gap-x-10'>
+          <li className='body-text text-gradient_blue-purple !font-bold'>
+            <Link href='https://jsmastery.pro/next13 target="_blank'>
+              Next.js 13.4 Course
+            </Link>
+          </li>
+          <li className='body-text !font-bold'>
+            <Link href='https://jsmastery.pro/masterclass target="_blank'>
+              Masterclass
+            </Link>
+          </li>
 
         </ul>
 
